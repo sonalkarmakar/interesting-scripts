18cabc0a56e8b5573b9c0ebee0225e14addeae7f
Sonal Karmakar <sonalkarmakar.gaining400@slmails.com>
2026-01-01 16:11:29 +0530

    Updated comment formatting to have consistent spacing

--- Python/NumberInWords/NumberInWords.py
+++ Python/NumberInWords/NumberInWords.py
@@ -26,15 +26,15 @@ def three_digit_group (number_group):
 	# String for containing the group's words
 	three_digit_word = ""
 
-	ones_digit = number_group % 10			# Right-most digit
-	tens_digit = (number_group // 10) % 10	# Middle digit
-	hundreds_digit = number_group // 100	# Left-most digit
+	ones_digit = number_group % 10         # Right-most digit
+	tens_digit = (number_group // 10) % 10 # Middle digit
+	hundreds_digit = number_group // 100   # Left-most digit
 
 	# Separates the conditions for having a 0 as the left-most digit, like 023 and 123
 	if number_group >= 100:
-		hundred_word = "hundred and "	# When left-most digit isn't 0, i.e., 123
+		hundred_word = "hundred and " # When left-most digit isn't 0, i.e., 123
 	else:
-		hundred_word = ""				# When left-most digit is 0, i.e., 023
+		hundred_word = ""             # When left-most digit is 0, i.e., 023
 
 	# Checks for numbers from 10 to 19
 	if tens_digit == 1:
@@ -46,23 +46,23 @@ def three_digit_group (number_group):
 	return three_digit_word
 
 # Main Function starts here
-number = input_number	# Copy the input number for modifying
-group_count = 0			# Counts the number of 3-digit groups
+number = input_number # Copy the input number for modifying
+group_count = 0       # Counts the number of 3-digit groups
 
 # This section takes the right-most 3-digit group, converts that to words, and finally removes that group from the end of the number
 # The steps are continued as long as 'number' is not reduced to 0 because of division
 while number > 0:
-	group_count = group_count + 1	# Increase the count of 3-digit groups by 1
+	group_count = group_count + 1 # Increase the count of 3-digit groups by 1
 
 	# [1]	(number % 1000) sends the right-most 3 digits to the three_digit_group () function
 	# [2]	Get the name of the 3-digit group in words
 	# [3]	Append the words of the previous 3-digit group at the end
-	if number % 1000 == 0:	# If it's all 0s in a group, don't append anything (eg.: 1,000,000 -> One Million)
+	if number % 1000 == 0:  # If it's all 0s in a group, don't append anything (eg.: 1,000,000 -> One Million)
 		output_words = three_digit_group (number % 1000) + output_words
-	else:					# If there are numbers, convert them to words
+	else:                   # If there are numbers, convert them to words
 		output_words = three_digit_group (number % 1000) + period_groups[group_count] + output_words
 
-	number = number // 1000	# Remove the right-most 3 digits as they've been processed
+	number = number // 1000 # Remove the right-most 3 digits as they've been processed
 
 # Final output of the given number in words
 print (output_words.rstrip (" ,"))
